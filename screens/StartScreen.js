40ad3406aadd75d5e1aa42771399bc06cf71c254
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-02-29 23:57:04 +0300

    text input configured

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -4,7 +4,11 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartScreen(){
     return(
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} />
+            <TextInput style={styles.numberInput}
+             maxLength={2}
+             keyboardType="number-pad"
+             autoCapitalize="none"
+             autoCorrect={false} />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
