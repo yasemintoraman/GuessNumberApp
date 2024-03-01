95740e0ab1f97ffeb08628326886d0d256fd8fea
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-03-01 18:21:37 +0300

    linear gradient added

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import StartScreen from './screens/StartScreen';
+import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import StartScreen from "./screens/StartScreen";
 
 export default function App() {
-  return <StartScreen/>;
+  return (
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <StartScreen />
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
-
+  rootScreen: {
+    flex: 1,
+  },
 });
