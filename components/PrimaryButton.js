a719fe11e310611d9226b4b0069fdc75e7a0c72f
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-03-01 16:58:28 +0300

    Pressable and some styling added

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,33 @@
-import {View, Text} from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton({children}){
-    return(
-        <View>
-            <Text>{children}</Text>
-        </View>
-    );
+function PrimaryButton({ children }) {
+
+  function pressHandler() {
+    console.log('Pressed!!');
+  }
+
+  return (
+    <Pressable onPress={pressHandler} android_ripple={{color: '#72063c'}}>
+      <View sytle={styles.buttonContainer}>
+        <Text style = {styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
+  );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        backgroundColor: '#72063c',
+        borderRadius: 28,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        margin: 4,
+        elevation: 2,
+    },
+    buttonText:{
+        color: 'white',
+        textAlign: 'center',
+    },
+});
