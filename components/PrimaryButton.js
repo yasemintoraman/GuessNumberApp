b8f408f1f2a09ab4ccfa0043dafbd0cdb3e253bc
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-03-01 17:36:55 +0300

    buttons improved

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,33 +1,45 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
-
   function pressHandler() {
-    console.log('Pressed!!');
+    console.log("Pressed!!");
   }
 
   return (
-    <Pressable onPress={pressHandler} android_ripple={{color: '#72063c'}}>
-      <View sytle={styles.buttonContainer}>
-        <Text style = {styles.buttonText}>{children}</Text>
-      </View>
-    </Pressable>
+    <View sytle={styles.buttonOuterContainer}>
+      <Pressable 
+        style={({ pressed }) => 
+        pressed ? [styles.buttonInnerContainer, styles.pressed]
+        : styles.buttonInnerContainer}
+        onPress={pressHandler} 
+        android_ripple={{ color: "#640233" }}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
   );
 }
 
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        backgroundColor: '#72063c',
-        borderRadius: 28,
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-        margin: 4,
-        elevation: 2,
-    },
-    buttonText:{
-        color: 'white',
-        textAlign: 'center',
-    },
+
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed:{
+    opacity: 0.75
+  }
 });
--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,44 +1,60 @@
 import { View, TextInput, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartScreen(){
-    return(
-        <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput}
-             maxLength={2}
-             keyboardType="number-pad"
-             autoCapitalize="none"
-             autoCorrect={false} />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+function StartScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
         </View>
-    );
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 export default StartScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        marginTop: 100,
-        marginHorizontal: 24,
-        padding: 16,
-        backgroundColor: '#4e0329',
-        borderRadius: 8,
-        elevation: 4,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.25
-      },
-      numberInput: {
-        height: 50,
-        width: 50,
-        fontSize: 32,
-        borderBottomColor: '#ddb52f',
-        borderBottomWidth: 2,
-        color: '#ddb52f',
-        marginVertical: 8,
-        fontWeight: 'bold',
-        textAlign: 'center'
-      }
+  inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
+  }
 });
