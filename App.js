6f60b6253581b9e2d18c9a0fbafad87a135b461d
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-03-13 01:38:25 +0300

    custom fonts added and used

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import {useFonts} from 'expo-font';
+import AppLoading from "expo-app-loading";
 
 import StartScreen from "./screens/StartScreen";
 import GuessScreen from "./screens/GuessScreen";
@@ -10,6 +12,15 @@ import EndScreen from "./screens/EndScreen";
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+
+  if( !fontsLoaded){
+    return <AppLoading />;
+  }
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
