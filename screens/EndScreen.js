8e7e39186ba699d486e7baa4782fc400003d35a1
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-03-14 15:42:42 +0300

    some style changes

--- screens/EndScreen.js
+++ screens/EndScreen.js
@@ -1,7 +1,56 @@
-import {Text} from 'react-native';
+import { View, Image, Text, StyleSheet } from "react-native";
 
-function EndScreen(){
-    return <Text>Your guess chances are over!</Text>
+import Title from '../components/ui/Title';
+import Colors from '../constants/colors';
+
+function EndScreen() {
+  return (
+  <View style={styles.rootContainer}>
+    <Title>GAME OVER!</Title>
+    <View style={styles.imageContainer}> 
+      <Image 
+        sytle={styles.image} 
+        source={require('../assets/images/success.png')} 
+        />
+    </View>
+    <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to 
+        guess the number <Text style={styles.highlight}>Y</Text>.
+    </Text>
+  </View>
+  );
 }
 
 export default EndScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden'
+        margin: 36,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    summaryText: {
+        fontFamily: 'open-sans',
+        fontSize: 24,
+        textAlign: 'center',
+        marginBottom: 24
+    },
+    highlight: {
+        fontFamily: 'open-sans-bold',
+        color: Colors.primary500,
+    },
+});
